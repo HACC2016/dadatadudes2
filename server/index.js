db9cb50d2c7d9cc33230e35e6cc9a40762a0eb69
Alexander Anich <alexander.anich@gmail.com>
2016-09-17 14:53:23 -1000

    addsd fix to webpack server and seed files

--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import graphqlHTTP from 'express-graphql';
 import assert from 'assert';
 import { MongoClient } from 'mongodb';
 import path from 'path';
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.config.js');
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 
-
+import config from '../webpack.config.js';
 import mdbConstructor from './mongodb';
 import schema from './schema';
 
@@ -51,12 +50,12 @@ MongoClient.connect(process.env.MONGO_URL, (err, mPool) => {
       })
     }))(req, res);
   });
-  
+
   if (DEBUG) {
     const compiler = webpack(config);
     const middleware = webpackMiddleware(compiler, {
       publicPath: config.output.publicPath,
-      contentBase: 'src',
+      contentBase: './src',
       stats: {
         colors: true,
         hash: false,
@@ -69,18 +68,17 @@ MongoClient.connect(process.env.MONGO_URL, (err, mPool) => {
 
     app.use(middleware);
     app.use(webpackHotMiddleware(compiler));
-    app.get('*', function response(req, res) {
-      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    app.get('*', (req, res) => {
+      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
       res.end();
     });
   } else {
-    app.use(express.static(__dirname + '/dist'));
-    app.get('*', function response(req, res) {
-      res.sendFile(path.join(__dirname, 'dist/index.html'));
+    app.use(express.static(path.join(__dirname, '../dist')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
   }
   app.listen(port, () => {
     console.log(`Node app is running on port ${port}`);
   });
-
 });
