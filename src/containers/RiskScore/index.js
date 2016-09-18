b93ec3625e455821c15eae99c6dab89cf72549d3
Brock Lanoza <brock.lanoza@hpe.com>
2016-09-18 12:20:24 -1000

    Add menu with island names to RiskScore container

--- src/containers/RiskScore/index.js
+++ src/containers/RiskScore/index.js
@@ -1,9 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-import { riskScore as actions } from './module';
-import { connect } from 'react-redux';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Article from 'grommet/components/Article';
-import Section from 'grommet/components/Section';
+import Box from 'grommet/components/Box';
+import Menu from 'grommet/components/Menu';
+import Header from 'grommet/components/Header';
+import Heading from 'grommet/components/Heading';
+import Anchor from 'grommet/components/Anchor';
+import Chart from 'grommet/components/Chart';
+import Base from 'grommet/components/Chart/Base';
+import Bar from 'grommet/components/Chart/Bar';
 
 class RiskScore extends Component {
   constructor(props) {
@@ -16,17 +23,48 @@ class RiskScore extends Component {
 
   render() {
     return (
-      <Article>
-        <Section align="center" justify="center">
-
-        </Section>
+      <Article pad={{vertical: 'small'}}>
+        <Heading align="center" tag="h4">Islands</Heading>
+        <Box 
+          align="center" 
+          justify="center">
+          <Menu inline={true} direction="row">
+            <Anchor>Oahu</Anchor>
+            <Anchor>Maui</Anchor>
+            <Anchor>Hawaii</Anchor>
+            <Anchor>Kauai</Anchor>
+          </Menu>
+        </Box>
       </Article>
     );
   }
 }
 
-export const stateToProps = state => ({
-  ...state
-});
+// const personsQuery = 
+//   gql`
+//   query {
+//     persons {
+//       _id
+//       firstName
+//       lastName
+//       assessmentIds
+//       assessments {
+//         _id
+//         personId
+//         overallRiskScore
+//         preSurveyScore
+//         historyOfHousingAndHomelessnessScore
+//         risksScore
+//         socializingAndDailyFunctionsScore
+//         wellnessScore
+//       }
+//     }
+//   } `;
 
-export default connect(stateToProps, actions)(RiskScore);
+// const listPersons = graphql(personsQuery, {
+//   props: ({ data }) => ({ 
+//     response: data ? data : []
+//   })
+// });
+
+export default RiskScore;
